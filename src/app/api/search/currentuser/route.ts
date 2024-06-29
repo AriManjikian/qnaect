d3090c8ac1ec32ce13f27fa69ce345846013c961
AriManjikian <arikev2007@gmail.com>
2024-06-28 23:45:25 -0400

    experimenting

--- src/app/api/search/currentuser/route.ts
+++ src/app/api/search/currentuser/route.ts
@@ -5,20 +5,20 @@ import User from "@/models/user";
 import { authConfig } from "@/lib/auth";
 
 export async function GET() {
-  try {
-    const session = await getServerSession(authConfig);
-    await connectMongoDB();
-    const user = await User.findOne({ email: session?.user?.email });
-    if (!user) {
-      return NextResponse.json(null, { status: 404 });
-    }
+  // try {
+  //   const session = await getServerSession(authConfig);
+  //   await connectMongoDB();
+  //   const user = await User.findOne({ email: session?.user?.email });
+  //   if (!user) {
+  //   }
 
-    return NextResponse.json(user, { status: 200 });
-  } catch (error) {
-    console.log(error);
-    return NextResponse.json(
-      { message: "An error occured", error },
-      { status: 500 }
-    );
-  }
+  //   return NextResponse.json(user, { status: 200 });
+  // } catch (error) {
+  //   console.log(error);
+  //   return NextResponse.json(
+  //     { message: "An error occured", error },
+  //     { status: 500 }
+  //   );
+  // }
+  return NextResponse.json(null, { status: 404 });
 }
